0b9a94aac8ebcc77abeac988a99a1ed83b7d5cc4
laerkeloenborg <lalo0001@stud.kea.dk>
2026-05-25 11:07:35 +0200

    csv file removed from weekly_update.py

--- frontend/sider/Weekly_update.py
+++ frontend/sider/Weekly_update.py
@@ -2,28 +2,47 @@ from components.bar_chart import bar_chart
 import numpy as np
 import streamlit as st
 import pandas as pd
-import os
+
+from services.food_api import get_meals
+
 
 def show_weekly_page():
-    
 
-    file_name = "foodlog.csv"
+    meals_data = get_meals()
 
-    if os.path.exists(file_name):
-        df = pd.read_csv(file_name)
+    if meals_data:
+
+        df = pd.DataFrame(meals_data)
+
         df["Date"] = pd.to_datetime(df["Date"])
 
-        last_7_days = df[df["Date"] >= (pd.Timestamp.today() - pd.Timedelta(days=7))]
-        daily_kcal = last_7_days.groupby("Date")["Calories"].sum()
+        last_7_days = df[
+            df["Date"] >= (
+                pd.Timestamp.today() - pd.Timedelta(days=7)
+            )
+        ]
 
-        
+        daily_kcal = last_7_days.groupby("Date")[
+            "Calories"
+        ].sum()
+
         col1, col2 = st.columns([3, 1])
+
         avg_kcal = np.mean(daily_kcal.values)
-        
+
         with col1:
+
             fig = bar_chart(daily_kcal)
+
             st.pyplot(fig)
 
-        
         with col2:
-            st.metric("Your weekly average", f"{round(avg_kcal, 0)} kcal")
+
+            st.metric(
+                "Your weekly average",
+                f"{round(avg_kcal, 0)} kcal"
+            )
+
+    else:
+
+        st.info("No meal data available yet.")
